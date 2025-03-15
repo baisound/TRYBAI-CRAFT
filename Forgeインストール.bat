9138dc6a72e4f9a0a3814cb829cc928fb8db3a54
はきみて れく(瞭魅推 れく) <hackimitelec@gmail.com>
2025-03-15 23:59:13 +0900

    バッチファイル同封

---
+++ Forgeインストール.bat
@@ -0,0 +1,2 @@
+@echo off
+.\jdk-21.0.4+7\bin\java.exe -jar forge-1.20.1-47.3.0-installer.jar
---
+++ 起動.bat
@@ -0,0 +1,3 @@
+@echo off
+.\jdk-21.0.4+7\bin\java.exe -Xmx3G -Xms3G -jar mohist-1.20.1-859-server.jar
+pause
